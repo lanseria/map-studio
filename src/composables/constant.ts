f99b63fd40d4d20a9484ded637b676e5889eee5e
lanseria <zhangchao564265135@hotmail.com>
2023-08-31 14:28:46 +0800

    chore

--- src/composables/constant.ts
+++ src/composables/constant.ts
@@ -223,6 +223,10 @@ export const PANGU_TIMELINE_IMG_LIST = [
     label: 'mslp-wind850_2023-08-30_00',
     url: '/2023-08-30_00.json',
   },
+  {
+    label: 'mslp-wind850_2023-08-30_12',
+    url: '/2023-08-30_12.json',
+  },
 ]
 
 export const MAP_DATA_STORM_FORECAST_PANGU_GIF_SOURCE = 'storm-forecast-pangu-gif-source'
