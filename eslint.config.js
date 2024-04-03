a387216cda881bdb11765838c89c0b8e497e5c2e
lanseria <zhangchao564265135@hotmail.com>
2024-04-03 14:04:26 +0800

    update deps and add git hooks

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   unocss: true,
-  files: ['**/*.{ts,js,jsx,vue,mjs}'],
-  formatters: {
-    css: true, // by default use Prettier
-    html: true, // by default use Prettier
-    toml: 'dprint', // use dprint for TOML
-    markdown: 'prettier', // use prettier for markdown
-  },
+  formatters: true,
   ignores: [
     'node_modules/',
     'dist/',
